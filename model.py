90b0a28e4d7618da13cb43850c5bcb58991b8b66
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-05 23:29:01 +0000

    Created model.py file and created users table

---
+++ model.py
@@ -0,0 +1,42 @@
+"""Models for Family Ties App"""
+
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+# Replace this with your code!
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+    
+
+class User(db.model):
+    """A user"""
+    
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    profile_id = db.Column(db.Integer,
+                        autoincrement= True)
+    full_name = db.Column(db.String)
+    DOB = db.Column(db.datetime)
+    current_locationd= db.Column(db.String)
+    place_of_birth = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
