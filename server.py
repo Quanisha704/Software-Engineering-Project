784589efdec4621d6560d54804775b17084985b8
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-07 18:34:21 +0000

    no major changes made just deleted some items on the page

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 #from random import choice
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
@@ -14,6 +14,9 @@ def homepage():
     """Prompts the user to sign up or sign in"""
     
     return render_template("welcomepage.html")
+  
+  
+
 
 
 if __name__ == '__main__':
