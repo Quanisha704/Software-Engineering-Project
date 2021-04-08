60784fc2c0981c1a97632dbd825e51f597d1ae69
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-08 18:59:57 +0000

    added for loop to generate random user event ids from the userevent table in model.py and tested output

--- seed.py
+++ seed.py
@@ -3,23 +3,42 @@ from faker import Faker
 import os 
 import server
 
+#dropping and creating database called family 
 os.system('dropdb family') 
 os.system('createdb family')
 
+#connecting to server.py and creating all databases
 connect_to_db(server.app)
 db.create_all()
 
+#Creating an instance(obj) of Faker 
 fake = Faker()
-users = []
 
-for i in range(10):
-    u = User(email = fake.email(), password = fake.password(),
-                    name = fake.name())
-    users.append(u)
-print(users)
+#Create users from user table by generating fake data
+#and store them in a list so we can use them 
+#to create user accounts and profiles 
+# users = []
+
+# for i in range(10):
+#     u = User(email = fake.email(), 
+#             password = fake.password(),
+#             name = fake.name(),
+#             dob = fake.date_of_birth())
+#     users.append(u)
+# print(users)
+
+
+#Create user event from the user event table
+user_event=[]
+for i in range(5):
+    ue = UserEvent(userevent_id = fake.random_number())
+    user_event.append(ue)
+print(user_event)
     
 
+#Create event from the event table
 
-db.session.add_all(users) 
-# # some users are implicitly added by relationship to questions
+
+
+db.session.add_all(user_event) 
 db.session.commit()
