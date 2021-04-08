037338638c23cb192f258cc09234052ee4be208e
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-08 00:13:35 +0000

    populated database with User class from model.py with a for loop and imported Faker library to generate fake data

--- seed.py
+++ seed.py
@@ -9,10 +9,17 @@ os.system('createdb family')
 connect_to_db(server.app)
 db.create_all()
 
+fake = Faker()
+users = []
+
+for i in range(10):
+    u = User(email = fake.email(), password = fake.password(),
+                    name = fake.name())
+    users.append(u)
+print(users)
+    
 
 
-
-
-# db.session.add_all()() 
+db.session.add_all(users) 
 # # some users are implicitly added by relationship to questions
-# db.session.commit()
+db.session.commit()
