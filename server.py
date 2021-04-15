91bb568b173de1b60bf8d7a7a336383cd4bb220b
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-15 05:59:11 +0000

    revised sign in route and created sign in using post route

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-"""Family Ties Flask app."""
+"""Server for Family Ties Flask app."""
 
-from flask import Flask, request, render_template, redirect, request, session
+from flask import (Flask, request, render_template, redirect, session, flash)
+from model import connect_to_db
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+import crud
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
@@ -12,38 +14,66 @@ app.jinja_env.auto_reload = True
 app.secret_key = "FAMILY"
 
 
+
+
+################################### LANDING PAGE, REGISTER, SIGN IN, SIGN OUT ##############################
 @app.route('/')
 def homepage():
     """Homepage for app to ask the user to sign in or register"""
     
     return render_template('welcomepage.html')    
-   
 
-
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    """Prompts the user to register."""
-    
-    # if 'name' or 'email' in session:  #if name or email already exist redirect to sign in page
-    #     return redirect('/sign_in')
-    # else:
-    #     return render_template('register.html')
-    
 
 @app.route('/sign_in')
-def login():
-    """Prompts the user to sign in"""
+def sign_in_form():
+    """Prompts the user to sign in"""   
     
-    # email = request.args.get('email')
-    # session['email'] = email 
-    # password = request.args.get('password')
-    # session['password'] = password     
+    return render_template('sign_in.html')
+
+
+@app.route('/sign_in', methods= ['POST'])
+def sign_in_post():
+    """Grabs the information from sign form"""
     
-    return render_template("sign_in.html")
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    
+    # user = crud.get_user_by_email(email)
+    # if email == user.email:
+    #     session['user_id'] = user_id
+    #     session['email'] = email
+    #     session['password'] = password
+        
+    return render_template('user_profile.html')
 
 
 
+# @app.route('/register')
+# def register_form():
+#     """Prompts the user to register for new account."""
+    
+#     return render_template('register.html') 
 
+
+# @app.route('/register', methods = ['POST'])
+# def register_post():
+#     """Prompts the user to register."""
+    
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     name = request.form.get('name')
+
+#     user = crud.get_user_by_email(email)
+#     if user:
+#         flash('An account with the email address already exist. Please try again.')
+#     else:
+#         crud.create_user(email, password, name, dob, isAdmin)
+#         flash('Account created! Please sign in.')
+
+#     return redirect('/')
+
+################################### PROFILE , CALENDAR
 # @app.route('/profile')
 # def profile():
 #     """Displays user profile information"""
