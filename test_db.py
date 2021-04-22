54a283c7e355c434289718b19cf871e1fb2af3eb
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-22 23:12:53 +0000

    testing User class - output hangs when ran

--- test_db.py
+++ test_db.py
@@ -29,51 +29,45 @@ class TestUser(unittest.TestCase):
                                      current_location = 'Texas', dob ='2000-12-09', place_of_birth = 'South Carolina', isAdmin =False)
        
        
-
     #Testing to make sure that the user object has been created is an instance of the User class
     def test_user_creation(self):
         self.assertIsInstance(self.user, User)
-
+        
     #Testing that the user object 'email' has been constructed properly 
     def test_email(self):
         self.assertEqual('shelly@time.com', self.user.email)
         
-    # #Testing that the user object 'password' has been constructed properly 
-    # def test_pw(self):
-    #     expected_pw = self.user.password
-    #     self.assertEqual(expected_pw, self.user.password)
+    #Testing that the user object 'password' has been constructed properly 
+    def test_pw(self):
+        self.assertEqual('rY742&1@', self.user.password)
    
-    # #Testing that the user object 'name' has been constructed properly 
-    # def test_name(self):
-    #     expected_name = self.user.name
-    #     self.assertEqual(expected_name, self.user.name)
+    #Testing that the user object 'name' has been constructed properly 
+    def test_name(self):
+        self.assertEqual('Shelly Green', self.user.name)
     
-    # #Testing that the user object 'current location' has been constructed properly 
-    # def test_current_location(self):
-    #     expected_CL= self.user.current_location
-    #     self.assertEqual(expected_CL, self.user.current_location)
+    #Testing that the user object 'current location' has been constructed properly 
+    def test_current_location(self):
+        self.assertEqual('Texas', self.user.current_location)
     
-    # #Testing that the user object 'dob' has been constructed properly 
-    # def test_dob(self):
-    #     expected_dob= self.user.dob
-    #     self.assertEqual(expected_dob, self.user.dob)
+    #Testing that the user object 'dob' has been constructed properly 
+    def test_dob(self):
+        self.assertEqual('2000-12-09', self.user.dob)
     
-    # #Testing that the user object 'place of birth' has been constructed properly 
-    # def test_pob(self):
-    #     expected_pob= self.user.place_of_birth
-    #     self.assertEqual(expected_pob, self.user.place_of_birth)
+    #Testing that the user object 'place of birth' has been constructed properly 
+    def test_pob(self):
+        self.assertEqual('South Carolina', self.user.place_of_birth)
     
-    # #Testing that the user object 'isAdmin' has been constructed properly 
-    # def test_admin(self):
-    #     expected_admin= self.user.isAdmin
-    #     self.assertEqual(expected_admin, self.user.isAdmin)
+    #Testing that the user object 'isAdmin' has been constructed properly 
+    def test_admin(self):
+        self.assertEqual(False, self.user.isAdmin)
+    
     
     def tearDown(self):
         db.drop_all()
-         
-    
-  
-   
+        
+        
+if __name__ == " __main__":
+    unittest.main()
    
    
    
