7914603a4b504435dfc093c1d543063942467105
Quanisha Williams <ms_q_williams@yahoo.com>
2021-04-24 01:10:11 +0000

    testing

--- test_server.py
+++ test_server.py
@@ -29,19 +29,39 @@ class FlaskTestsBasic(TestCase):
         self.assertIn(b"Welcome To Family Ties", result.data)
         self.assertEqual(result.status_code, 200)
     
-    # def test_signIn(self):
-    #     """Test sign in page."""
+    def test_signIn(self):
+        """Test sign in page."""
 
-    #     result = self.client.post("/sign_in",
-    #                               data={"email": "beasleyshelly@yahoo.com", "password": "TTV9zm^ip*"}, follow_redirects=True)
-    #     self.assertIn(b"Sign in successful!", result.data)
+        result = self.client.post("/sign_in",
+                                  data={"email": "beasleyshelly@yahoo.com", "password": "TTV9zm^ip*"}, follow_redirects=True)
+        self.assertIn(b"Sign in successful!", result.data)
         
     def test_register(self):
         """Testing register page"""
         
         result = self.client.get("/register")
         self.assertEqual(result.status_code, 200)
-        
+
+# class FlaskTestsLoggedIn(TestCase):
+#     """Flask tests with user logged in to session."""
+
+#     def setUp(self):
+#         """Stuff to do before every test."""
+
+#         app.config['TESTING'] = True
+#         app.config['SECRET_KEY'] = 'testingKey'
+#         self.client = app.test_client()
+
+#         with self.client as c:
+#             with c.session_transaction() as sess:
+#                 sess['email'] = "beasleyshelly@yahoo.com"
+
+#     def test_profile_page(self):
+#         """Test profile page."""
+
+#         result = self.client.get("/user_profile")
+#         self.assertIn(b"Your profile details are below:", result.data)
+
 
 if __name__ == "__main__":
     import unittest
